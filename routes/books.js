decf1cb2502939fd9c51596ce85ba487b8bf1fc7
Thomas <thomas@craftacademy.se>
2020-06-09 13:47:15 +0200

    removes ref from users to books

--- routes/books.js
+++ routes/books.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.json({users: [{name: 'Timmy'}]});
+router.get('/', (req, res) => {
+  res.json({books: [{title: 'The Bible'}]});
 });
 
 module.exports = router;
