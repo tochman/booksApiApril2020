139133a42cfbe851b53560ad42d2278eface1327
Thomas <thomas@craftacademy.se>
2020-06-10 11:23:55 +0200

    adds and configures sequelize adds books model with migration and seed

---
+++ database/config/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "development": {
+    "username": "postgres",
+    "password": null,
+    "database": "books_api_april_development",
+    "host": "127.0.0.1",
+    "dialect": "postgres",
+    "operatorsAliases": false
+  },
+  "test": {
+    "username": "postgres",
+    "password": null,
+    "database": "books_api_april_test",
+    "host": "127.0.0.1",
+    "dialect": "postgres",
+    "operatorsAliases": false
+  }
+}
